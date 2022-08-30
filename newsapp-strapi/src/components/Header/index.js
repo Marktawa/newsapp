fa880d24b5934426560eddee17122e2f525b6df7
McTawa <markmunyaka@gmail.com>
2022-08-30 21:21:28 +0000

    Build the components: Header

---
+++ newsapp-strapi/src/components/Header/index.js
@@ -0,0 +1,11 @@
+// /newsapp/newsapp-strapi/src/components/Header/index.js
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <section className="header">
+      <div className="headerName">News24</div>
+    </section>
+  );
+}
